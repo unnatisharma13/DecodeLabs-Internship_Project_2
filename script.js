52863a6018ac80e68f67610c0beb4d4323ab86c4
unnatisharma13 <unnatisharma1309@gmail.com>
2026-06-11 01:33:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuBtn =
+document.querySelector(".menu-btn");
+
+const navLinks =
+document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+navLinks.classList.toggle("active");
+});
